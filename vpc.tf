3620d3670740b5e677897b39777b659a68837bbf
ravi2krishna <ravi2krishna@gmail.com>
2023-03-15 02:10:17 +0000

    create subnet

--- vpc.tf
+++ vpc.tf
@@ -8,3 +8,13 @@ resource "aws_vpc" "lms" {
   }
 }
 
+# Subnet
+resource "aws_subnet" "lms-subnet" {
+  vpc_id                  = aws_vpc.lms.id
+  cidr_block              = "10.0.1.0/24"
+  map_public_ip_on_launch = "true"
+
+  tags = {
+    Name = "lms-subnet"
+  }
+}
