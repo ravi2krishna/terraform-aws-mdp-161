4c1415b587fa1a487d08d18af0fa63ae1fb30085
ravi2krishna <ravi2krishna@gmail.com>
2023-03-15 02:43:40 +0000

    security group rules added

--- firewalls.tf
+++ firewalls.tf
@@ -45,3 +45,31 @@ resource "aws_security_group" "lms-web-sg" {
     Name = "lms-web-sg"
   }
 }
+
+# Security Group Rules
+resource "aws_security_group_rule" "lms-ssh-rule" {
+  type              = "ingress"
+  from_port         = 22
+  to_port           = 22
+  protocol          = "tcp"
+  cidr_blocks       = ["0.0.0.0/0"]
+  security_group_id = aws_security_group.lms-web-sg.id
+}
+
+resource "aws_security_group_rule" "lms-http-rule" {
+  type              = "ingress"
+  from_port         = 80
+  to_port           = 80
+  protocol          = "tcp"
+  cidr_blocks       = ["0.0.0.0/0"]
+  security_group_id = aws_security_group.lms-web-sg.id
+}
+
+resource "aws_security_group_rule" "lms-outbound-rule" {
+  type              = "egress"
+  from_port         = 0
+  to_port           = 65535
+  protocol          = "tcp"
+  cidr_blocks       = ["0.0.0.0/0"]
+  security_group_id = aws_security_group.lms-web-sg.id
+}
